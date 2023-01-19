2e7e98d66a53f7d9d103f2de96e4ca16f64e5a08
PavFet <fetiskinpavel@gmail.com>
2023-01-19 14:49:59 +0200

    feat: rendering CarForm component

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,14 +1,15 @@
-import CarsCollection from '../helpers/cars-collection';
 import Table from './table';
 import cars from '../data/cars';
 import brands from '../data/brands';
 import models from '../data/models';
+import CarsCollection from '../helpers/cars-collection';
 import stringifyProps, { StringifyObjectProps } from '../helpers/stringify-object';
 import CarJoined from '../types/car-joined';
 import SelectField from './select-field';
+import CarForm from './car-form';
 
 class App {
-  private htmlElement: HTMLElement;
+  private carsCollection: CarsCollection;
 
   private selectedBrandId: null | string;
 
@@ -16,76 +17,101 @@ class App {
 
   private carTable: Table<StringifyObjectProps<CarJoined>>;
 
-  private carsCollection: CarsCollection;
+  private carForm: CarForm;
+
+  private htmlElement: HTMLElement;
 
   public constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
-    this.carsCollection = new CarsCollection({ cars, brands, models });
 
-    this.carTable = new Table({
-      title: 'Car list',
-      columns: {
-        id: 'ID',
-        brand: 'Brand',
-        model: 'Model',
-        price: 'Price',
-        year: 'Year',
-      },
-      rowsData: this.carsCollection.allCars.map(stringifyProps),
-      onDelete: this.handleCarDelete,
-    });
-    this.brandSelect = new SelectField({
-      labelText: 'Brands',
-      options: [
-        ...brands.map(({ id, title }) => ({ title, value: id })),
-        { title: 'All brands', value: '-1' },
-      ],
-      onChange: this.handleBrandChange,
-    });
     this.selectedBrandId = null;
 
     this.htmlElement = foundElement;
 
-    this.initialize();
+    this.carsCollection = new CarsCollection({ cars, brands, models });
+
+    this.carTable = new Table({
+      title: 'Visi automobiliai',
+      columns: {
+        id: 'Id',
+        brand: 'Markė',
+        model: 'Modelis',
+        price: 'Kaina',
+        year: 'Metai',
+      },
+      rowsData: this.carsCollection.all.map(stringifyProps),
+      onDelete: this.handleCarDelete,
+    });
+
+    this.brandSelect = new SelectField({
+      labelText: 'Markė',
+      options: [
+        { title: 'Visi automobiliai', value: '-1' },
+        ...brands.map(({ id, title }) => ({ title, value: id })),
+      ],
+      onChange: this.handleBrandChange,
+    });
+
+    const initialBrandId = brands[0].id;
+    this.carForm = new CarForm({
+      title: 'Sukurkite naują automobilį',
+      submitBtnText: 'Sukurti',
+      values: {
+        brand: initialBrandId,
+        model: models.filter((m) => m.brandId === initialBrandId)[0].id,
+        price: '0',
+        year: '2000',
+      },
+      onSubmit: () => { },
+    });
   }
 
-  private handleBrandChange = (brandId: string): void => {
-    this.selectedBrandId = brandId;
-    this.update();
+  private handleBrandChange = (brandId: string) => {
+    const brand = brands.find((b) => b.id === brandId);
+    this.selectedBrandId = brand ? brandId : null;
+
+    this.renderView();
   };
 
-  private handleCarDelete = (carId: string): void => {
+  private handleCarDelete = (carId: string) => {
     this.carsCollection.deleteCarById(carId);
 
-    this.update();
+    this.renderView();
   };
 
-  private update = (): void => {
+  private renderView = () => {
     const { selectedBrandId, carsCollection } = this;
 
     if (selectedBrandId === null) {
       this.carTable.updateProps({
-        title: 'All cars',
-        rowsData: carsCollection.allCars.map(stringifyProps),
+        title: 'Visi automobiliai',
+        rowsData: carsCollection.all.map(stringifyProps),
       });
     } else {
       const brand = brands.find((b) => b.id === selectedBrandId);
-      if (brand === undefined) throw new Error('Brand is not exist');
+      if (brand === undefined) throw new Error('Pasirinkta neegzistuojanti markė');
 
       this.carTable.updateProps({
-        title: `${brand.title} Car brand`,
+        title: `${brand.title} markės automobiliai`,
         rowsData: carsCollection.getByBrandId(selectedBrandId).map(stringifyProps),
       });
     }
   };
 
   public initialize = (): void => {
+    const uxContainer = document.createElement('div');
+    uxContainer.className = 'd-flex gap-4 align-items-start';
+    uxContainer.append(
+      this.carTable.htmlElement,
+      this.carForm.htmlElement,
+    );
+
     const container = document.createElement('div');
-    container.className = 'container my-3';
+    container.className = 'container my-4 d-flex flex-column gap-4';
     container.append(
       this.brandSelect.htmlElement,
-      this.carTable.htmlElement,
+      uxContainer,
     );
 
     this.htmlElement.append(container);
--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,7 +1,7 @@
-import type Car from '../types/car';
-import type Model from '../types/model';
-import type Brand from '../types/brand';
-import type CarJoined from '../types/car-joined';
+import Car from '../types/car';
+import Model from '../types/model';
+import Brand from '../types/brand';
+import CarJoined from '../types/car-joined';
 
 type CarsCollectionProps = {
   cars: Car[],
@@ -9,24 +9,34 @@ type CarsCollectionProps = {
   models: Model[],
 };
 
-class CarsCollection {
-  private props: CarsCollectionProps;
+export type CarProps = {
+  brandId: string,
+  modelId: string,
+  price: number,
+  year: number
+};
 
-  public constructor(props: CarsCollectionProps) {
-    this.props = props;
-  }
+const createId = (): string => String(Math.floor(Math.random() * 100000000000000));
+
+class CarsCollection {
+  constructor(private props: CarsCollectionProps) { }
 
   private joinCar = ({ modelId, ...car }: Car) => {
     const { brands, models } = this.props;
+
     const carModel = models.find((model) => model.id === modelId);
     const carBrand = brands.find((brand) => brand.id === carModel?.brandId);
 
     return {
       ...car,
-      brand: (carBrand && carBrand.title) ?? 'unknown',
-      model: (carModel && carModel.title) ?? 'unknown',
+      brand: (carBrand && carBrand?.title) ?? 'unknown',
+      model: (carModel && carModel?.title) ?? 'unknown',
     };
   };
+
+  public get all(): CarJoined[] {
+    return this.props.cars.map(this.joinCar);
+  }
 
   public getByBrandId = (brandId: string): CarJoined[] => {
     const { cars, models } = this.props;
@@ -42,12 +52,26 @@ class CarsCollection {
     return brandCars;
   };
 
-  public get allCars(): CarJoined[] {
-    return this.props.cars.map(this.joinCar);
-  }
-
   public deleteCarById = (carId: string): void => {
     this.props.cars = this.props.cars.filter((car) => car.id !== carId);
+  };
+
+  public add = ({ modelId, brandId, ...carProps }: CarProps): void => {
+    const { models, brands, cars } = this.props;
+    const model = models.find((m) => m.id === modelId);
+    const brand = brands.find((b) => b.id === brandId);
+
+    if (!model || !brand) {
+      throw new Error('Netinkami duomenys sukurti automobilį');
+    }
+
+    const newCar: Car = {
+      id: createId(),
+      ...carProps,
+      modelId,
+    };
+
+    cars.push(newCar);
   };
 }
 
